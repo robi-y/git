20f675487957d5bb3ec127d3f761b282c5b213e4
Derrick Stolee <dstolee@microsoft.com>
2021-05-14 18:31:06 +0000

    unpack-trees: stop recursing into sparse directories
    When walking trees using traverse_trees_recursive() and
    unpack_callback(), we must not attempt to walk into a sparse directory
    entry. There are no index entries within that directory to compare to
    the tree object at that position, so skip over the entries of that tree.
    
    This code is used in many places, so the only way to test it is to start
    removing the command_requres_full_index option from one builtin at a
    time and carefully test that its use of unpack_trees() behaves correctly
    with a sparse-index. Such tests will be added by later changes.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- unpack-trees.c
+++ unpack-trees.c
@@ -1262,6 +1262,7 @@ static int unpack_callback(int n, unsigned long mask, unsigned long dirmask, str
 	struct cache_entry *src[MAX_UNPACK_TREES + 1] = { NULL, };
 	struct unpack_trees_options *o = info->data;
 	const struct name_entry *p = names;
+	unsigned unpack_tree = 1;
 
 	/* Find first entry with a real name (we could use "mask" too) */
 	while (!p->mode)
@@ -1308,7 +1309,8 @@ static int unpack_callback(int n, unsigned long mask, unsigned long dirmask, str
 		}
 	}
 
-	if (unpack_nondirectories(n, mask, dirmask, src, names, info) < 0)
+	if (unpack_tree &&
+	    unpack_nondirectories(n, mask, dirmask, src, names, info) < 0)
 		return -1;
 
 	if (o->merge && src[0]) {
@@ -1338,7 +1340,8 @@ static int unpack_callback(int n, unsigned long mask, unsigned long dirmask, str
 			}
 		}
 
-		if (traverse_trees_recursive(n, dirmask, mask & ~dirmask,
+		if (unpack_tree &&
+		    traverse_trees_recursive(n, dirmask, mask & ~dirmask,
 					     names, info) < 0)
 			return -1;
 		return mask;
